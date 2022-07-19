d98b479a11649fc276b5ad1eb16598fde8df9b72
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-19 21:46:55 +0900

    add : headers, prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -10,4 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+void	*ft_memmove(void *dst, const void *src, int len);
+
+#endif
