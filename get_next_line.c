c3f3f927974af31af482a0f9937b2ebbb6f71716
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-19 21:46:25 +0900

    add : get_next_line input check, read method

--- get_next_line.c
+++ get_next_line.c
@@ -10,4 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	char	*temp_read;
+	char	*result;
+	int		read_size;
+	int		i;
+
+	if (fd < 0 || fd > OPEN_MAX)
+		return (NULL);
+	temp_read = (char *) malloc(BUFFER_SIZE);
+	read_size = read(fd, temp_read, BUFFER_SIZE);
+	i = 0;
+	while (i + 1 < read_size && *(temp_read + i) != '\n')
+		++i;
+	if (*(temp_read + i) == '\n')
+		result = (char *) malloc(i);
+	else
+		result = (char *) malloc(i);
+	ft_memmove(result, temp_read, i);
+	return (result);
+}
