a7279ac057a790147b502e1e9c73345001c59325
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-07-20 16:57:21 +0900

    add : const char parameter

--- get_next_line.c
+++ get_next_line.c
@@ -14,6 +14,7 @@
 
 char	*get_next_line(int fd)
 {
+	const char	*temp_left = NULL;
 	char	*temp_read;
 	char	*result;
 	int		read_size;
@@ -22,14 +23,24 @@ char	*get_next_line(int fd)
 	if (fd < 0 || fd > OPEN_MAX)
 		return (NULL);
 	temp_read = (char *) malloc(BUFFER_SIZE);
+	if (!temp_read)
+		return (NULL);
 	read_size = read(fd, temp_read, BUFFER_SIZE);
-	i = 0;
-	while (i + 1 < read_size && *(temp_read + i) != '\n')
-		++i;
+	i = -1;
+	while (++i < read_size)
+	{
+		if (*(temp_read + i) == '\n')
+		{
+			result = (char *) malloc(i);
+			ft_memmove(result, temp_read, i);
+			break ;
+		}
+	}
 	if (*(temp_read + i) == '\n')
 		result = (char *) malloc(i);
 	else
 		result = (char *) malloc(i);
 	ft_memmove(result, temp_read, i);
+	free(temp_read);
 	return (result);
 }
